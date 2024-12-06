cfb12d8a059c8c03655e7d3bf443e5d71a5580f9
IraCalvo <ira.zephaniah.calvo@gmail.com>
2024-12-05 21:03:22 -0800

    Stun + Slow status effects

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -7,33 +7,37 @@ public class EnemyMovement : MonoBehaviour
     EnemyAIBrain enemyAIBrain;
     Enemy enemy;
     Vector2 enemyMoveDir;
+    StatusEffects statusEffects;
 
     private void Awake()
     {
         enemyAIBrain = GetComponent<EnemyAIBrain>();
         enemy = GetComponent<Enemy>();
+        statusEffects = GetComponent<StatusEffects>();
     }
 
     private void Update()
     {
-        if (enemyAIBrain.currentTarget != null)
+        if (statusEffects.isStunned == false)
         {
-            //Debug.Log("current target not null");
-            float distance = Vector2.Distance(enemyAIBrain.currentTarget.position, transform.position);
-            if (distance > enemy.enemySO.enemyAttackRange)
+            if (enemyAIBrain.currentTarget != null)
             {
-                enemyMoveDir = enemyAIBrain.GetDirectionToMove();
-                transform.position += enemy.enemySO.enemyBaseMS * Time.deltaTime * (Vector3)enemyMoveDir;
+                float distance = Vector2.Distance(enemyAIBrain.currentTarget.position, transform.position);
+                if (distance > enemy.enemySO.enemyAttackRange)
+                {
+                    enemyMoveDir = enemyAIBrain.GetDirectionToMove();
+                    transform.position += (enemy.enemySO.enemyBaseMS * Time.deltaTime * (Vector3)enemyMoveDir) * statusEffects.slowAmount;
+                }
+                else
+                {
+                    //do attack stuff, can add delay to check so that its not ran every frame
+                    enemy.enemyState = EnemyState.Attacking;
+                }
             }
-            else 
+            else if (enemyAIBrain.GetTargetsCount() > 0)
             {
-                //do attack stuff, can add delay to check so that its not ran every frame
-                enemy.enemyState = EnemyState.Attacking;
+                enemyAIBrain.currentTarget = enemyAIBrain.targets[0];
             }
-        }
-        else if (enemyAIBrain.GetTargetsCount() > 0)
-        {
-            enemyAIBrain.currentTarget = enemyAIBrain.targets[0];
         }
     }
 }
--- Assets/Scripts/Managers/StatusEffects.cs
+++ Assets/Scripts/Managers/StatusEffects.cs
@@ -5,8 +5,44 @@ public class StatusEffects : MonoBehaviour
 {
     public bool isOnFire;
     public bool isStunned;
+    public bool isSlowed;
+
+    public float slowAmount = 1f;
 
     float elapsedBurnTime;
+    float elapsedStunTime;
+    float elapsedSlowTime;
+
+    IDamageable healthScript;
+
+    private void Awake()
+    {
+        healthScript = GetComponent<IDamageable>();
+    }
+
+    public void ApplyStun(float stunDuration)
+    {
+        if (isStunned == false)
+        {
+            StartCoroutine(StunStatusCoroutine(stunDuration));
+        }
+        else
+        {
+            elapsedStunTime = 0f;
+        }
+    }
+
+    public void ApplySlow(float slowAmount ,float slowDuration)
+    {
+        if (isSlowed == false)
+        {
+            StartCoroutine(SlowStatusCoroutine(slowAmount, slowDuration));
+        }
+        else
+        {
+            elapsedSlowTime = 0f;
+        }
+    }
 
     public void ApplyBurn(int burnDamage, float burnDuration)
     {
@@ -30,7 +66,7 @@ public class StatusEffects : MonoBehaviour
             burnTickTimer += Time.deltaTime;
             if (burnTickTimer >= IntUtils.DefaultDebufAndBuffTimers.DefaultBurnInterval)
             {
-                gameObject.GetComponent<IDamageable>().TakeDamage(burnDamage, DamageSource.Neutral, 0);
+                healthScript.TakeDamage(burnDamage, DamageSource.Neutral, 0);
                 burnTickTimer = 0f;
             }
 
@@ -39,5 +75,29 @@ public class StatusEffects : MonoBehaviour
         }
 
         isOnFire = false;
+    }
+
+    IEnumerator StunStatusCoroutine(float stunDuration)
+    {
+        while (elapsedStunTime < stunDuration)
+        {
+            isStunned = true;
+            elapsedStunTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isStunned = false;
+    }
+
+    IEnumerator SlowStatusCoroutine(float slowPercent, float slowDuration)
+    {
+        while (elapsedSlowTime < slowDuration)
+        {
+            slowAmount = slowPercent;
+            elapsedSlowTime += Time.deltaTime;
+            yield return null;
+        }
+
+        slowAmount = 1f;
     }
 }
